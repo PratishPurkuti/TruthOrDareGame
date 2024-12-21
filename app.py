3d13f8111b0f609252bf177ffa1a87dec84d10c6
Pratish <85995556+PratishPurkuti@users.noreply.github.com>
2024-12-21 11:33:55 +0545

    minor changes to the game
    added and improved the
    1) counter of players
    2) Players Name

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, session , request
+from flask import Flask, render_template, session , request ,redirect
 
 
 app = Flask(__name__)
+app.secret_key = 'kaloseto'
 
 @app.route('/')
 def index():
@@ -10,10 +11,23 @@ def index():
 @app.route('/gameload', methods=['POST', 'GET'])
 def gameload():
     if request.method == 'POST':
-        Number = request.form.get('Numbers')
+        try:
+            Number = int(request.form.get('number'))
+        except (ValueError, TypeError):
+            # Handle cases where the input is not a valid integer
+            return "Invalid input. Please enter a number."
+        if 2 <= Number <= 15:
+            session['Number'] = Number
+            return render_template('playername.html',Number=Number)
 
     return render_template("gameload.html")
 
+@app.route('/playername',methods=['POST','GET'])
+def playername():
+    if request.method =='POST':
+        Number = session.get('Number')
+        return render_template('Gamemode.html')
+    return render_template('playername.html',Number=Number)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)   
